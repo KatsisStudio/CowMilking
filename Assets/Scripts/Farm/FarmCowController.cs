b0b7050db3bf1e8925fe5c159bd43fe3dffd6934
Xwilarg <xwilarg@protonmail.com>
2024-02-12 14:33:15 +0100

    Add cow wandering script

---
+++ Assets/Scripts/Farm/FarmCowController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CowMilking.Farm
+{
+    public class FarmCowController : MonoBehaviour
+    {
+        private Rigidbody2D _rb;
+        private SpriteRenderer _sr;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _sr = GetComponent<SpriteRenderer>();
+
+            StartCoroutine(WalkAround());
+        }
+
+        private IEnumerator WalkAround()
+        {
+            while (true)
+            {
+                _rb.velocity = Random.insideUnitCircle.normalized * 1f;
+                yield return new WaitForSeconds(Random.Range(1f, 3f));
+            }
+        }
+
+        private void Update()
+        {
+            _sr.sortingOrder = (int)(-transform.position.y * 100f + 10_000_000f);
+        }
+    }
+}
