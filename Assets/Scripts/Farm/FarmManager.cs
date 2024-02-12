76864bcb30a47b76f512f5e2a8af8c6e4d86ade2
Xwilarg <xwilarg@protonmail.com>
2024-02-12 14:53:42 +0100

    Add farm cow spawner

---
+++ Assets/Scripts/Farm/FarmManager.cs
@@ -0,0 +1,26 @@
+﻿using CowMilking.Persistency;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CowMilking.Farm
+{
+    public class FarmManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _cowPrefab;
+
+        private void Awake()
+        {
+            SceneManager.LoadScene("CowManager", LoadSceneMode.Additive);
+        }
+
+        private void Start()
+        {
+            foreach (var cow in PersistencyManager.Instance.SaveData.OwnedCows)
+            {
+                var go = Instantiate(_cowPrefab, Vector2.zero, Quaternion.identity);
+                go.GetComponent<SpriteRenderer>().sprite = CowManager.Instance.GetCow(cow).Sprite;
+            }
+        }
+    }
+}
